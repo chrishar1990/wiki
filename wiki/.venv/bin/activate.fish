3eb86b33c466264e37f82d2325f9262f4b5dcef6
Chris Harris <g4network@gmail.com>
2025-05-10 13:43:26 -0400

    updating project

--- wiki/.venv/bin/activate.fish
+++ wiki/.venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/g4network/cs50W/projects/wiki/wiki/.venv"
+set -gx VIRTUAL_ENV "/home/g4network/webDevPython/wiki/wiki/.venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,8 +1,18 @@
+from django import forms
 from django.http import HttpResponse
 from django.shortcuts import render
 
 from . import util
 
+#tasks_list  = []
+
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label="Title")
+    content_text_area = forms.CharField(
+        label="Content", 
+        widget=forms.Textarea(attrs={'rows': 10, 'cols': 10
+        ,'class': 'my-textarea' }),)
+    
 
 def index(request):
     
@@ -10,10 +20,62 @@ def index(request):
         "entries": util.list_entries()
     })
 
+tasks_list = []
+
+
+
+def create_page(request):
+    print("View function is running!")  # Debugging: Confirm the view is being called
+    allEntries = util.list_entries()
+    entryExist = False
+    for entry in allEntries:
+        print(entry.upper())
+    
+
+
+    if request.method == "POST":
+        
+        print("Form submitted!")  # Debugging: Confirm the form is being submitted
+        form = NewTaskForm(request.POST)
+        
+        #entryExist = False
+
+        if form.is_valid():
+            
+            print("Form is valid!")  # Debugging: Confirm the form is valid
+            task_title = form.cleaned_data["task"]
+            task_content = form.cleaned_data["content_text_area"]
+            tasks_list.append({"title": task_title, "content": task_content})
+            print(f"Task added: {task_title}  \n Content added:  {task_content}")  # Debugging: Confirm the task is added
+            # Redirect or perform some other action
+            for entry in allEntries:
+                if task_title.upper() == entry.upper():
+                    entryExist = True
+                    
+            
+            if(entryExist == False):
+                util.save_entry(task_title, task_content)
+            else:
+                print("Entry already EXIST!!!")
+            
+        else:
+            print("Form is invalid!")  # Debugging: Confirm the form is invalid
+            return render(request, "encyclopedia/create_page.html", {
+                "form": form
+            })
+    
+    return render(request, "encyclopedia/create_page.html", {
+        "form": NewTaskForm(),
+        "entry": util.list_entries(),
+        "entryExist": entryExist
+        
+    })
+
 
 
 def entry_page(request, title):
-    print("What I typed: " + title)  
+    print("What I typed: did it changed? " + title) 
+    
     entry = util.get_entry(title)
     print(entry)
     if entry == None:
@@ -22,6 +84,7 @@ def entry_page(request, title):
         if entry == None:
             return error(request, title)
         else:
+            print("Rendering entry_page.html1")
             return render(request, "encyclopedia/entry_page.html",{
              "entry": entry,
               "title": title
@@ -29,6 +92,8 @@ def entry_page(request, title):
         
        
     else:
+        print("Rendering entry_page.html2")
+
         return render(request, "encyclopedia/entry_page.html",{
              "entry": entry,
               "title": title
